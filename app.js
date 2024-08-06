db919c0afbae359d36f85ab907b061cac81c091b
Bruno Leonardo Ramos Ribeiro <louzeiroribeiro@gmail.com>
2024-08-06 20:04:36 -0300

    Exceptions

--- app.js
+++ app.js
@@ -1,15 +1,38 @@
 
-// Funções tradicionais
-function saudacao(){
-    console.log(this.mensagem);
+// // Funções tradicionais
+// function saudacao(){
+//     console.log(this.mensagem);
+// }
+
+// const  exibir = ()=> console.log(this);
+
+// const obj = {
+//     mensagem: "Bem vindo!",
+//     exibirArrow: exibir,
+// }
+
+// obj.exibirArrow();
+
+// TRY CATCH
+// try {
+//     console.log(varivale);
+// } catch (error) {
+//     console.log('Ocorreu um erro');
+// } finally {
+//     console.log('Finalizado');
+// }
+
+
+function verificarIdade(idade) {
+    if (idade < 18 ){
+        throw Error("Idade deve ser maior que 18");
+    }
+
+    return "Idade Correta";
 }
 
-const  exibir = ()=> console.log(this);
-
-const obj = {
-    mensagem: "Bem vindo!",
-    exibirArrow: exibir,
+try {
+    console.log(verificarIdade(19));
+} catch (error) {
+    console.log(error);
 }
-
-obj.exibirArrow();
-
